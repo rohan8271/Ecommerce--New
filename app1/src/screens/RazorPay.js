e09541fa60de14bf1903dd20895f61a3a7939b2d
Rohan Saha <36517636+saharohan@users.noreply.github.com>
2021-09-23 00:16:43 +0530

    Update RazorPay.js

--- app1/src/screens/RazorPay.js
+++ app1/src/screens/RazorPay.js
@@ -102,7 +102,7 @@ export default function RazorPay(props) {
     //dispatch(payOrder(order /*paymentResult*/));
 
     const options = {
-      key: "rzp_test_aEKLjl6zuBJKqc", // Enter the Key ID generated from the Dashboard
+      key: "?", // Enter the Key ID generated from the Dashboard
       amount: order.totalPrice * 100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
       currency: "INR",
       name: order.shippingAddress.fullName,
